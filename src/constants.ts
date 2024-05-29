32193d685720375b17371f092289d9486ada42d2
KameleoonTraining <jnickason+1@kameleoon.com>
2024-05-29 13:43:16 -0700

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "2p2rgz4sbp";
+export const siteCode = "34d06nwtb4";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
